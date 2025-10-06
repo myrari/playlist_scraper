91b1cd9d365fe9bd9dcc4aabe3f0280895724795
Myra Roberts <myra@myrari.net>
2025-10-05 19:03:49 -0700

    FAR too much work, but the web version works!

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { randomBytes } from "crypto";
+import { stringify } from "querystring";
 
 const app = express();
 const port = process.env.PORT;
+
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+const redirect_uri = "http://127.0.0.1:4242/callback";
 
 const root = dirname(fileURLToPath(import.meta.url))
 
@@ -23,8 +30,62 @@ app.get("/main.js", (_req, res) => {
 
 app.get("/client_id", (_req, res) => {
     res.appendHeader("Content-Type", "text/plain");
-    res.send(process.env.SPOTIFY_CLIENT_ID);
+    res.send();
 })
+
+app.get('/login', (_req, res) => {
+
+    const state = randomBytes(8).toString("hex");
+    const scope = "playlist-read-private playlist-read-collaborative";
+
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
+});
+
+app.get("/callback", (req, res) => {
+    const code = req.query.code;
+    const state = req.query.state;
+
+    if (state == undefined) {
+        res.redirect("/#" +
+            stringify({
+                error: "state_mismatch"
+            }));
+    } else {
+        fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            body: stringify({
+                code: code?.toString(),
+                redirect_uri: redirect_uri,
+                grant_type: "authorization_code",
+            }),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic " +
+                    (Buffer.from(
+                        client_id + ":" + client_secret).toString("base64")
+                    )
+            },
+        }).then((resp) => resp.json().then((body) => {
+            const access_token = body.access_token;
+            const expires_in = body.expires_in;
+
+            res.redirect("http://127.0.0.1:4242/?" +
+                stringify({
+                    access_token: access_token,
+                    expire_time:
+                        Math.floor(new Date().getTime() / 1000 + expires_in),
+                })
+            );
+        }));
+    }
+});
 
 app.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}`);
--- server/src/main.js
+++ server/src/main.js
@@ -1,4 +1,157 @@
-async function get_client_id() {
-    const id = await (await fetch("client_id")).text()
-    document.getElementById("button_resp_txt").innerHTML = id
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+const access_token = params.access_token;
+const expire_time = params.expire_time;
+
+let user_text_elem = null;
+
+addEventListener("DOMContentLoaded", _ => {
+    const time = Math.floor(new Date().getTime() / 1000);
+    const remaining = expire_time - time;
+
+    const main_interact_elem = document.getElementById("main_interact");
+    user_text_elem = document.getElementById("user_text");
+
+    if (access_token == null || remaining <= 0) {
+        console.log("no token yet or expired");
+
+        main_interact_elem.innerHTML = "<form action='http://127.0.0.1:4242/login' method='get'><button type='submit'>Login</button></form>";
+
+    } else {
+        console.log(`token!! (${remaining} remaining)`);
+
+        main_interact_elem.innerHTML =
+            "<label for='time_input'>Show Start Time: </label>" +
+            "<input type='time' id='time_input'>" +
+            "<input type='checkbox' id='use_name_checkbox'>" +
+            "<input type='text' id='name_textbox'>" +
+            "<button type='submit' id='search_button'>Search</button>";
+
+        const use_name_checkbox = document.getElementById("use_name_checkbox");
+        const name_textbox = document.getElementById("name_textbox");
+        const search_button = document.getElementById("search_button");
+
+        use_name_checkbox.onchange = _ => {
+            name_textbox.disabled = !this.checked;
+        };
+
+        search_button.onclick = _ => {
+            if (use_name_checkbox.checked) {
+                search_playlist();
+            } else {
+                if (!name_textbox.value) {
+                    user_text_elem.innerHTML = "Please specify a playlist name!";
+                } else {
+                    search_playlist(name_textbox.value);
+                }
+            }
+        };
+    }
+});
+
+async function search_playlist(name = "") {
+    const playlists = await (
+        await fetch("https://api.spotify.com/v1/me/playlists", {
+            headers: {
+                Authorization: "Bearer " + access_token,
+            }
+        })
+    ).json();
+
+    // const today = new Date(2025, 9, 29);
+    const today = new Date();
+
+    let playlist = null;
+    if (name) {
+        const filtered = playlists.items.filter((p) => {
+            const p_name = p.name.trim().toLowerCase();
+            const s_name = name.trim().toLowerCase();
+            const index = p_name.indexOf(s_name);
+            return index >= 0;
+        });
+
+        if (filtered.length < 1) {
+            user_text_elem.innerHTML = "No playlist found!";
+        } else {
+            playlist = filtered[0];
+        }
+    } else {
+        const filtered = playlists.items.filter((p) => {
+            const p_name = p.name.trim().toLowerCase();
+            const dates = p_name.match(/\d+\/\d+\/\d+/);
+            if (dates) {
+                for (const d of dates) {
+                    const split = d.split("/");
+                    if (split[0] == today.getMonth()
+                        && split[1] == today.getDate()) {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        });
+
+        if (filtered.length < 1) {
+            user_text_elem.innerHTML = "No playlist found!";
+        } else {
+            playlist = filtered[0];
+        }
+    }
+
+    if (!playlist) {
+        user_text_elem.innerHTML = "No playlist found!";
+        return;
+    }
+    user_text_elem.innerHTML = `Found playlist ${playlist.name}!`;
+
+
+    const tracks = await (
+        await fetch(
+            `https://api.spotify.com/v1/playlists/${playlist.id}/tracks/?` +
+            "fields=items(track(artists(name),name,duration_ms))",
+            {
+                headers: {
+                    Authorization: "Bearer " + access_token,
+                }
+            })
+    ).json();
+
+    let formatted = [];
+
+    let milis_elapsed = 0;
+
+    for (const t of tracks.items) {
+        const track = t.track;
+        const name = track.name;
+        const artist = track.artists[0].name;
+
+        const length = track.duration_ms;
+        const start_milis = milis_elapsed;
+        milis_elapsed += length;
+        let start_time = new Date(today);
+
+        const time_input = document.getElementById("time_input");
+        if (time_input.value) {
+            const split = time_input.value.split(":");
+            start_time.setHours(split[0]);
+            start_time.setMinutes(split[1]);
+        }
+
+        start_time = new Date(start_time.getTime() + start_milis);
+        const time_str = start_time.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+
+        formatted.push(`<p style='margin:unset'>${artist} - ${name} - ${time_str}</p>`);
+    }
+
+    user_text_elem.innerHTML += "<div " +
+        "style='display:flex;flex-direction:column;padding:10;background-color:lightgray;border:gray 5px;border-style:solid;border-radius:5px;font-weight:normal'" +
+        ">" +
+        (formatted.join("")) +
+        "</div>";
 }
